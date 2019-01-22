99883c8d2f3057fa74f3dadaaf7b38f981741886
Abhishek Patnaik <abhishekpatnaik77@gmail.com>
2019-01-22 18:39:06 +0530

    added login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 
@@ -8,6 +8,20 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("search.html")
+
+@app.route("/login")
+def load_login():
+    return render_template("login.html")
+
+
+@app.route('/login',methods=['POST'])
+def get_login_info():
+    firstName = request.form['username']
+    lastName = request.form['password']
+    print(firstName,lastName)
+    return "ok done"
+
+
 
 
 
